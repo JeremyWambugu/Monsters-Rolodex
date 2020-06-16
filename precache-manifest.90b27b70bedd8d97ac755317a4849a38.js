93bf2adecf4b33a3382f6b98e561f99055db8d66
jeremy wambugu <wambugujeremy@gmail.com>
2020-06-16 16:53:18 +0300

    Updates

---
+++ precache-manifest.90b27b70bedd8d97ac755317a4849a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732eaa1c692791eb7550097101d954f5",
+    "url": "/JeremyWambugu/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "df8eb923b7042f011c1b",
+    "url": "/JeremyWambugu/Monsters-Rolodex/static/css/main.70b6108b.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/JeremyWambugu/Monsters-Rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JeremyWambugu/Monsters-Rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8eb923b7042f011c1b",
+    "url": "/JeremyWambugu/Monsters-Rolodex/static/js/main.daacd39d.chunk.js"
+  },
+  {
+    "revision": "ff7f09b315d7986ff1f4",
+    "url": "/JeremyWambugu/Monsters-Rolodex/static/js/runtime-main.9c54ac2b.js"
+  }
+]);
